c092ee71ad79e3977e055ceb1d5fdfdc2767bb86
Gri25 <grigorijabrosimov@gmail.com>
2022-03-26 21:43:58 +0500

    feat: добавил модели и схемы

---
+++ app.js
@@ -0,0 +1,55 @@
+// require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+// const { errors } = require('celebrate');
+// const routesUser = require('./routes/users');
+// const routerCard = require('./routes/cards');
+// const { NotFoundErr } = require('./errors');
+// const auth = require('./middlewares/auth');
+// const { requestLogger, errorLogger } = require('./middlewares/Logger');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+// app.use(cors());
+
+app.use(express.json());
+
+// app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// app.use(routesUser);
+
+// app.use(routerCard);
+
+// app.use(auth);
+/*
+app.use((req, res, next) => {
+  next(new NotFoundErr('Не корректный URL'));
+});
+*/
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+/*
+app.use(errorLogger);
+
+app.use(errors());
+// здесь обрабатываем все ошибки
+app.use((err, req, res, next) => {
+//  console.log(err);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Ошибка на стороне сервера';
+  res.status(statusCode).send({ message });
+  //  res.status(500).send({ message: 'На сервере произошла ошибка' });
+
+  next();
+});
+*/
+app.listen(PORT);
